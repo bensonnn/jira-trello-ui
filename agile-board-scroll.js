a527950bee4be027cbc208fc79458ea28e89dd26
bensonnn <dan.s.benson@gmail.com>
2017-11-28 11:55:42 -0800

    css

--- agile-board-scroll.js
+++ agile-board-scroll.js
@@ -1,6 +1,7 @@
 const config = {
   scrollX: true,
-  deceleration: 0.001
+  deceleration: 0.001,
+  eventPassthrough: true
 }
 
 applyScroll()
@@ -9,9 +10,18 @@ let urlCache = window.location.href
 setInterval(function() {
   if (window.location.href !== urlCache) {
     urlCache = window.location.href
+    applyPropagationStop()
     applyScroll()
   }
 }, 100)
+
+function applyPropagationStop() {
+  document.getElementsByClassName('ghx-column').forEach((el) => {
+    el.on('drag', ev => ev.stopPropagation())
+    el.on('dragstart', ev => ev.stopPropagation())
+    el.on('dragend', ev => ev.stopPropagation())
+  })
+}
 
 function checkForSwimlane() {
   return document.getElementsByClassName('ghx-swimlane').length &&
